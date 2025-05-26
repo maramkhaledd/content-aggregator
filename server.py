324d78ca48e6a4ad5ce495352cb0d3717377f30a
SalmaaKhaledd <salmakhaledabdou@gmail.com>
2025-05-26 11:20:38 +0300

    setup TCP server

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
+import socket
+import pickle
+import requests
+from bs4 import BeautifulSoup
 
+
+def start_server():
+    print("Creating socket...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("binding socket...")
+    s.bind("localhost", 5050)
+    print("Listening for connections...")
+    s.listen(5)
+    print("Server is listening on port 5050...")
+
+    while True:
+        client_socket, addr = s.accept()
+        print(f"Connection from {addr} has been established.")
+
+
+if __name__ == "__main__":
+    start_server()
